5cc6975ef493a5acbcad05921863b2d400ebb08e
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2023-09-28 15:40:42 +0700

    feat: add player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace SpaceWar3D
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private FloatingJoystick _joyStick;
+        [SerializeField] private Animator _anim;
+        [SerializeField] private float _moveSpeed = 5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private Rigidbody rb;
+        private Vector3 _movement;
+
+        private void Update()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            _movement = new Vector3();
+            _movement.x = _joyStick.Horizontal * _moveSpeed * Time.deltaTime;
+            _movement.y = _joyStick.Vertical * _moveSpeed * Time.deltaTime;
+
+            rb.MovePosition(rb.position + _movement);
+            
+            // Debug.Log(_movement.x);
+
+            if (_movement.x < 0)
+            {
+                Debug.Log("Left");
+                _anim.Play("Left");
+            }
+            else if (_movement.x > 0)
+            {
+                Debug.Log("Right");
+                _anim.Play("Right");
+                // _anim.SetBool("isRight", true);
+                // _anim.SetBool("isLeft", false);
+            }
+            else
+            {
+                Debug.Log("Idle");
+                _anim.Play("Idle");
+            }
+        }
     }
 }
+
+
